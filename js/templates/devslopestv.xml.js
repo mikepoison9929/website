0df422c0c30b1c23d8e83c3649a7b6dcda8f15bc
mikepoison9929 <qygs3@msgos.com>
2016-01-07 18:14:11 +0000

    Create devslopestv.xml.js

---
+++ js/templates/devslopestv.xml.js
@@ -0,0 +1,267 @@
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+<document>
+<head>
+    <style>
+    .main {
+        background-color: #A198B7;
+    }
+</style>
+</head>
+  <listTemplate class="main">
+    <banner>
+      <background>
+        <img src="${this.BASEURL}resources/banner.jpg" width="1920" height="360"/>
+      </background>
+    </banner>
+    <list>
+      <section>
+       
+        <header>
+        <title>Movies</title>
+        </header>
+        
+        <listItemLockup vidUrl="http://2.bp.blogspot.com/sk2b-SSAHMF-9_-oxKKaCLh2d0ifmXaVuMlZ0-TlT_0D=m18">
+          <title>Star Wars: The Force Awakens (2015)</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://ia.media-imdb.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX640_SY720_.jpg"/>
+              <title>PG-13 | 2h 15min | Action, Adventure, Fantasy</title>
+              <subtitle>Daisy Ridley, John Boyega, Oscar Isaac</subtitle>
+              <description>Three decades after the defeat of the Galactic Empire, a new threat arises. The First Order attempts to rule the galaxy and only a ragtag group of heroes can stop them, along with the help of the Resistance.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+        <listItemLockup vidUrl="https://lh3.googleusercontent.com/L3STQuqq9SW4lOJYMvGjLi-zMbFII8TxN5HjFtickw=m37">
+          <title>Paper Towns (2015)</title>
+          <relatedContent>
+            <lockup>
+              <img src="https://nowshowingphilippines.files.wordpress.com/2015/08/paper-towns-2015.jpg"/>
+              <title>PG-13 | 1h 49min | Drama, Mystery, Romance</title>
+              <subtitle>Nat Wolff, Cara Delevingne, Austin Abrams</subtitle>
+              <description>After an all night adventure, Quentin's life-long crush, Margo, disappears, leaving behind clues that Quentin and his friends follow on the journey of a lifetime.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+        <listItemLockup vidUrl="https://lh3.googleusercontent.com/thaN8Xz-5ESHWAI8aiLfPCv5j94gd3n1u_BUCDRAPw=m37">
+          <title>Turbo Kid (2015)</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://www.tvqc.com/wp-content/uploads/2015/10/91lpelhDLAL._SL1500_.jpg"/>
+              <title>Unrated | 1h 33min | Action, Adventure, Sci-Fi </title>
+              <subtitle>Munro Chambers, Laurence Leboeuf, Michael Ironside</subtitle>
+              <description>In a post-apocalyptic wasteland, a comic book fan dons the persona of his favourite hero to save his enthusiastic friend and fight a tyrannical overlord.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+         <listItemLockup vidUrl="http://2.bp.blogspot.com/q1U-vMs1fMygV_EUP-RX8VCyDpoII9AxyEwf57fJewLF=m22">
+          <title>Pawn Sacrifice (2015)</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://ia.media-imdb.com/images/M/MV5BMTk0MDYxNTE0OV5BMl5BanBnXkFtZTgwODgyNjU3NTE@._V1_SX640_SY720_.jpg"/>
+              <title>PG-13 | 1h 55min | Biography, Drama</title>
+              <subtitle>Tobey Maguire, Liev Schreiber, Peter Sarsgaard</subtitle>
+              <description>Set during the Cold War, American chess prodigy Bobby Fischer finds himself caught between two superpowers and his own struggles as he challenges the Soviet Empire.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+       
+       
+        </section>
+      
+      
+        <section>
+        <header>
+        <title>Live TV</title>
+        </header>
+        
+        <listItemLockup vidUrl="http://ooyalahd2-f.akamaihd.net/i/crtv01_delivery@329705/index_1164_av-b.m3u8">
+          <title>Poker Central</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://www.poker-online.com/wp-content/uploads/2015/04/poker-central.jpg"/>
+              <title></title>
+              <subtitle></subtitle>
+              <description>Poker Central's® non-stop coverage of everything poker captivates viewers with thrilling victories...</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+       
+        <listItemLockup vidUrl="http://bfmtv-i.akamaihd.net/hls/live/218332/876450610001/bfmtv/index.m3u8">
+          <title>BFMTV</title>
+          <relatedContent>
+            <lockup>
+              <img src="https://upload.wikimedia.org/wikipedia/en/6/62/BFMTV.png"/>
+              <title></title>
+              <subtitle></subtitle>
+              <description>L'information en direct de France.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12netw-4728.m3u8">
+          <title>PAC-12 National</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+              <description>Live College Sports</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+                            
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12ariz-4728.m3u8">
+          <title>PAC-12 Arizona</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+           <description>Live College Sports</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+                
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12baya-4728.m3u8">
+          <title>PAC-12 Bay Area</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+             <description>Live College Sports</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+                
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12losa-4728.m3u8">
+          <title>PAC-12 Los Angeles</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+              <description>Live College Sports</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+                
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12moun-4728.m3u8">
+          <title>PAC-12 Mountain</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+            <description>Live College Sports</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+                
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12oreg-4728.m3u8">
+          <title>PAC-12 Oregon</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+             <description>Live College Sports</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+                
+          <listItemLockup vidUrl="http://xrxs.net/video/live-p12wash-4728.m3u8">
+          <title>PAC-12 Washington</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cdn1.theodysseyonline.com/files/2015/09/13/635777822728878846-1001491087_Pac-12%20South%20Football.png"/>
+             <description>Live College Sports</description>
+              <description></description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+         <listItemLockup vidUrl="http://nflioslivesvg2-i.akamaihd.net/hls/live/221521/nflioslive/2015HackerNow_w99d9.m3u8">
+          <title>NFL Now</title>
+          <relatedContent>
+            <lockup>
+              <img src="https://image.roku.com/blog/wp-content/uploads/2014/08/nfl-now.jpg"/>
+              <description>Sit back and catch up on the latest stories and trending topics across the National Football League with NFL Now Live.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+        <listItemLockup vidUrl="http://cbsnewshd-lh.akamaihd.net/i/CBSNHD_7@199302/master.m3u8">
+          <title>CBS News</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://cbsnews1.cbsistatic.com/hub/i/r/2012/08/31/b5fa43a4-3598-11e3-8ce8-047d7b15b92e/thumbnail/620x350/cbsnews_logo_2012.jpg"/>
+              <title></title>
+              <subtitle></subtitle>
+              <description>Live 24/7 News</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+         <listItemLockup vidUrl="http://edge1.everyon.tv/etv1sb/pld16/playlist.m3u8">
+          <title>CNN International</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://wepartypatriots.com/wp/wp-content/uploads/2014/09/CNN-logo.jpg"/>
+              <description>Live 24/7 News</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+            
+        </section>
+        
+         <section>
+        <header>
+        <title>Radios</title>
+        </header>
+        
+         <listItemLockup audUrl="http://master.dogglounge.com:9128/;stream/1">
+          <title>DoggLounge</title>
+          <relatedContent>
+            <lockup>
+              <img src="https://thumbnail.mixcloud.com/w/318/h/318/q/90/upload/images/extaudio/3b323d04-6795-4613-9fdd-8d4ff073c17e.jpg"/>
+              <title></title>
+              <subtitle></subtitle>
+              <description>Internet radio playing Deep House Music and more.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+                      
+        </section>
+        
+         <section>
+        <header>
+        <title>Adult</title>
+        </header>
+        
+         <listItemLockup vidUrl="http://109.71.162.112:1935/live/hd.jasminchannel.stream/playlist.m3u8">
+          <title>Jasmin.TV</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://www.spaceman-tvportal.de/KK/JASMIN.TV.jpg"/>
+              <title></title>
+              <subtitle></subtitle>
+              <description>Broadcasting live on our very own TV channel, with the prettiest hosts online!</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        
+        <listItemLockup vidUrl="http://178.21.120.198:1935/live3/_definst_/mp4:hustlerhd/playlist.m3u8">
+          <title>Hustler TV</title>
+          <relatedContent>
+            <lockup>
+              <img src="http://www.sat-address.com/f/pics/logotipi/Hustler-TV-Europe_o.jpg"/>
+              <title></title>
+              <subtitle></subtitle>
+              <description>From Hustler Adult Entertainment. Discover all the latest and best HD movies.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+                      
+        </section>
+    </list>
+  </listTemplate>
+</document>`
+}
